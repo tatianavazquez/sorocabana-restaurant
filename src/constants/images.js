a8460670c9e41b1333c0f540aa677789ae704344
Tati Vazquez <tatianavazquezr@hotmail.com>
2022-07-01 17:38:23 -0300

    Added photo gallery

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,10 +1,27 @@
 import bg from '../assets/bg.png';
 import chef from '../assets/chef.png';
 import G from '../assets/G.png';
-import gallery01 from '../assets/gallery01.png';
-import gallery02 from '../assets/gallery02.png';
-import gallery03 from '../assets/gallery03.png';
-import gallery04 from '../assets/gallery04.png';
+import gallery1 from '../assets/gallery1.png';
+import gallery2 from '../assets/gallery2.png';
+import gallery3 from '../assets/gallery3.png';
+import gallery4 from '../assets/gallery4.png';
+import gallery5 from '../assets/gallery5.png';
+import gallery6 from '../assets/gallery6.png';
+import gallery7 from '../assets/gallery7.png';
+import gallery8 from '../assets/gallery8.png';
+import gallery9 from '../assets/gallery9.png';
+import gallery10 from '../assets/gallery10.jpeg';
+import gallery11 from '../assets/gallery11.png';
+import gallery12 from '../assets/gallery12.png';
+import gallery13 from '../assets/gallery13.png';
+import gallery14 from '../assets/gallery14.jpeg';
+import gallery15 from '../assets/gallery15.jpeg';
+import gallery16 from '../assets/gallery16.jpeg';
+import gallery17 from '../assets/gallery17.jpeg';
+import gallery18 from '../assets/gallery18.jpeg';
+import gallery19 from '../assets/gallery19.jpeg';
+import gallery20 from '../assets/gallery20.jpeg';
+import gallery21 from '../assets/gallery21.jpeg';
 import knife from '../assets/knife.png';
 import logo from '../assets/logo.png';
 import menu from '../assets/menu.png';
@@ -12,23 +29,33 @@ import overlaybg from '../assets/overlaybg.png';
 import spoon from '../assets/spoon.svg';
 import welcome from '../assets/welcome.png';
 import findus from '../assets/findus.png';
-import laurels from '../assets/laurels.png';
-import award01 from '../assets/award01.png';
-import award02 from '../assets/award02.png';
-import award03 from '../assets/award03.png';
-import award05 from '../assets/award05.png';
-import sign from '../assets/sign.png';
-import quote from '../assets/quote.png';
 import gericht from '../assets/gericht.png';
 
 export default {
   bg,
   chef,
   G,
-  gallery01,
-  gallery02,
-  gallery03,
-  gallery04,
+  gallery1,
+  gallery2,
+  gallery3,
+  gallery4,
+  gallery5,
+  gallery6,
+  gallery7,
+  gallery8,
+  gallery9,
+  gallery10,
+  gallery11,
+  gallery12,
+  gallery13,
+  gallery14,
+  gallery15,
+  gallery16,
+  gallery17,
+  gallery18,
+  gallery19,
+  gallery20,
+  gallery21,
   knife,
   logo,
   menu,
@@ -36,12 +63,5 @@ export default {
   spoon,
   welcome,
   findus,
-  laurels,
-  award01,
-  award02,
-  award03,
-  award05,
-  sign,
-  quote,
   gericht,
 };
--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,97 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs'
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Gallery.css';
 
-const Gallery = () => (
-  <div>
-    Gallery
+const {
+  gallery1,
+  gallery2,
+  gallery3,
+  gallery4,
+  gallery5,
+  gallery6,
+  gallery7,
+  gallery8,
+  gallery9,
+  gallery10,
+  gallery11,
+  gallery12,
+  gallery13,
+  gallery14,
+  gallery15,
+  gallery16,
+  gallery17,
+  gallery18,
+  gallery19,
+  gallery20,
+  gallery21,
+} = images;
+
+const galleryImages = [
+  gallery1,
+  gallery2,
+  gallery3,
+  gallery4,
+  gallery5,
+  gallery6,
+  gallery7,
+  gallery8,
+  gallery9,
+  gallery10,
+  gallery11,
+  gallery12,
+  gallery13,
+  gallery14,
+  gallery15,
+  gallery16,
+  gallery17,
+  gallery18,
+  gallery19,
+  gallery20,
+  gallery21,];
+
+
+const Gallery = () => {
+
+  const scrollRef = useRef(null);
+
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if(direction === 'left') {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
+  };
+  
+  return (
+  <div className="app__gallery flex__center">
+    <div className="app__gallery-content">
+      <SubHeading title="Instagram" />
+      <h1 className="headtext__cormorant"> Galería de fotos </h1>
+      <p className="p__opensans" style={{ color: '#AAA', marginTop: '2rem'}}> Imágenes de nuestros deliciosos platos subidas a Instagram. Para seguirnos: @restaurantsorocabana</p>
+      <button type="button" className="custom__button"> Ver más </button>
+    </div>
+
+    <div className="app__gallery-images">
+      <div className="app__gallery-images_container" ref={scrollRef}>
+        {galleryImages.map((image, index) => (
+          <div className="app__gallery-images-card flex__center" key={`gallery_image-${index + 1}`}> 
+          <img src={image} alt="gallery"/>
+          <BsInstagram className="gallery__image-icon" />
+          </div>
+        ) )}
+      </div>
+      <div className="app__gallery-images-arrows">
+        <BsArrowLeftShort className="gallery__arrow-icon" onClick={() => scroll('left')} />
+        <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')} />
+      </div>
+
+    </div>
   </div>
-);
+)};
 
 export default Gallery;
